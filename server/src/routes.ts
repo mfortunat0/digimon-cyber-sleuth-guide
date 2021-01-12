971598edcf6c8eaf765d070c7181210a8dc1352c
matheus <matheus.xmaz10@gmail.com>
2021-01-12 14:53:09 -0300

    new path for interfaces

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,38 +1,10 @@
 import express from "express";
 import { readFileSync } from "fs";
-import path from "path";
-
-interface ISkill {
-    level: string;
-    name: string;
-    sp: string;
-    type: string;
-    power: string;
-    attribute: string;
-    inherit: string;
-    description: string;
-}
-
-interface IDigimon {
-    id: string;
-    name: string;
-    type: string;
-    memory: string;
-    requirements: string;
-    attribute: string;
-    equipSlot: string;
-    supSkillName: string;
-    supSkillDescribe: string;
-    digivolveFrom: string[];
-    digivolveInto: string[];
-    skills: ISkill[];
-}
+import { join } from "path";
+import IDigimon from "./interfaces/IDigimon";
 
 const data: IDigimon[] = JSON.parse(
-    readFileSync(
-        path.join(__dirname, "../data/digimon.json"),
-        "utf8"
-    ).toString()
+    readFileSync(join(__dirname, "../data/digimon.json"), "utf8").toString()
 );
 
 const router = express.Router();
