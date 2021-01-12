8e2cddf21bad64ecc47635b9a01fdd69e7f0055f
matheus <matheus.xmaz10@gmail.com>
2021-01-12 14:52:20 -0300

    Apollo server configured

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,24 @@
 import express from "express";
+import { ApolloServer } from "apollo-server";
+import { readFileSync } from "fs";
+import { join } from "path";
 import router from "./routes";
+import resolvers from "./resolvers";
+
+const typeDefs = readFileSync(
+    join(__dirname, "../schema.graphql"),
+    "utf8"
+).toString();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 const app = express();
 app.use(router);
 
 app.listen(3000, () => console.log("Rest on port 3000"));
+server.listen().then(() => {
+    console.log("Graphql on port 4000");
+});
