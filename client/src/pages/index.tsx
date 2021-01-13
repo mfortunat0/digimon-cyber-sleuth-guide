dd0e43db23b19ca06a75043fec6969f69c0a63c3
matheus <matheus.xmaz10@gmail.com>
2021-01-13 11:20:45 -0300

    Data obtained from the server applied to the frontend

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Head from "next/head";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import {
   Row,
   Button,
@@ -13,12 +16,24 @@ import {
   EvoltutionTitle,
   EvoltuionImages,
 } from "../styles/index";
-import Head from "next/head";
-import { useState } from "react";
+import IDigimon from "../interfaces/IDigimon";
 
 export default function Home() {
   const [describeShow, setDescribeShow] = useState(true);
   const [evolutionsShow, setEvolutionsShow] = useState(false);
+  const [digimon, setDigimon] = useState<IDigimon | undefined>();
+  const [data, setData] = useState<IDigimon[]>([]);
+  const textFieldRef = useRef<HTMLInputElement>(null);
+
+  const getData = async () => {
+    const result = await axios.get("http://localhost:3001/digimons");
+    setData(result.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <Head>
@@ -29,94 +44,116 @@ export default function Home() {
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Row justifyContent="center ">
-        <TextField list="digimons" />
-        <Button>Search</Button>
+      <Row justifyContent="center">
+        <TextField ref={textFieldRef} list="digimons" />
+        <Button
+          onClick={() => {
+            setDigimon(
+              data.find((value) => value.name === textFieldRef.current?.value)
+            );
+          }}
+        >
+          Search
+        </Button>
         <datalist id="digimons">
-          <option value="Agumon" />
+          {data &&
+            data.map((value, index) => {
+              return <option key={index} value={value.name} />;
+            })}
         </datalist>
       </Row>
-      <Row justifyContent="center">
-        <CardContainer>
-          <TabContainer>
-            <TabButton
-              onClick={() => {
-                setDescribeShow(true);
-                setEvolutionsShow(false);
-              }}
-            >
-              Describe
-            </TabButton>
-            <TabButton
-              onClick={() => {
-                setDescribeShow(false);
-                setEvolutionsShow(true);
-              }}
-            >
-              Evolutions
-            </TabButton>
-          </TabContainer>
+      {digimon && (
+        <>
           <Row justifyContent="center">
-            <ProfileImage source="/digimons/agumon.jpg" />
-          </Row>
-          <ProfileName>Agumon</ProfileName>
-          <DescribeContainer show={describeShow}>
-            <Row justifyContent="center">
-              <Describe>
-                <b>Stage: </b> Roockie
-              </Describe>
-            </Row>
-            <Row justifyContent="space-between">
-              <Describe>
-                <b>Type: </b>Vaccina
-              </Describe>
-              <Describe>
-                <b>Attribute: </b>Fire
-              </Describe>
-            </Row>
-            <Row justifyContent="space-between">
-              <Describe>
-                <b>Memory: </b>5
-              </Describe>
-              <Describe>
-                <b>Equip Slots: </b>1
-              </Describe>
-            </Row>
-            <Row justifyContent="flex-start">
-              <Describe>
-                <b>Support Skill: </b>
-                <i>Dragons Roar</i> <br />
-                Increases damage from Fire skills by 15%.
-              </Describe>
-            </Row>
-            <Row justifyContent="flex-start">
-              <Describe>
-                <b>Requires: </b>ATK: 30, CAM: 10%
-              </Describe>
-            </Row>
-          </DescribeContainer>
-          <Row justifyContent="center">
-            <EvolutionContainer show={evolutionsShow}>
-              <EvoltutionTitle>Digivolves From</EvoltutionTitle>
+            <CardContainer>
+              <TabContainer>
+                <TabButton
+                  onClick={() => {
+                    setDescribeShow(true);
+                    setEvolutionsShow(false);
+                  }}
+                >
+                  Describe
+                </TabButton>
+                <TabButton
+                  onClick={() => {
+                    setDescribeShow(false);
+                    setEvolutionsShow(true);
+                  }}
+                >
+                  Evolutions
+                </TabButton>
+              </TabContainer>
               <Row justifyContent="center">
-                <EvoltuionImages source="/digimons/greymon.jpg" />
-                <EvoltuionImages source="/digimons/geogreymon.jpg" />
-                <EvoltuionImages source="/digimons/meramon.jpg" />
-                <EvoltuionImages source="/digimons/tyrannomon.jpg" />
+                <ProfileImage
+                  source={`http://localhost:3001/images/${digimon?.name.toLowerCase()}.jpg`}
+                />
               </Row>
-            </EvolutionContainer>
-            <EvolutionContainer show={evolutionsShow}>
-              <EvoltutionTitle>Digivolves Into</EvoltutionTitle>
+              <ProfileName>{digimon?.name}</ProfileName>
+              <DescribeContainer show={describeShow}>
+                <Row justifyContent="center">
+                  <Describe textTransform="capitalize">
+                    <b>Stage: </b> {digimon?.stage}
+                  </Describe>
+                </Row>
+                <Row justifyContent="space-between">
+                  <Describe textTransform="capitalize">
+                    <b>Type: </b>
+                    {digimon?.type}
+                  </Describe>
+                  <Describe textTransform="capitalize">
+                    <b>Attribute: </b>
+                    {digimon?.attribute}
+                  </Describe>
+                </Row>
+                <Row justifyContent="space-between">
+                  <Describe textTransform="capitalize">
+                    <b>Memory: </b>
+                    {digimon?.memory}
+                  </Describe>
+                  <Describe textTransform="capitalize">
+                    <b>Equip Slots: </b>
+                    {digimon?.equipSlot}
+                  </Describe>
+                </Row>
+                <Row justifyContent="flex-start">
+                  <Describe textTransform="none">
+                    <b>Support Skill: </b>
+                    <i>{digimon?.supSkillName}</i> <br />
+                    {digimon?.supSkillDescribe}
+                  </Describe>
+                </Row>
+                <Row justifyContent="flex-start">
+                  <Describe textTransform="none">
+                    <b>Requires: </b>
+                    {digimon?.requirements}
+                  </Describe>
+                </Row>
+              </DescribeContainer>
               <Row justifyContent="center">
-                <EvoltuionImages source="/digimons/greymon.jpg" />
-                <EvoltuionImages source="/digimons/geogreymon.jpg" />
-                <EvoltuionImages source="/digimons/meramon.jpg" />
-                <EvoltuionImages source="/digimons/tyrannomon.jpg" />
+                <EvolutionContainer show={evolutionsShow}>
+                  <EvoltutionTitle>Digivolves From</EvoltutionTitle>
+                  <Row justifyContent="center">
+                    <EvoltuionImages source="/digimons/greymon.jpg" />
+                    <EvoltuionImages source="/digimons/geogreymon.jpg" />
+                    <EvoltuionImages source="/digimons/meramon.jpg" />
+                    <EvoltuionImages source="/digimons/tyrannomon.jpg" />
+                  </Row>
+                </EvolutionContainer>
+                <EvolutionContainer show={evolutionsShow}>
+                  <EvoltutionTitle>Digivolves Into</EvoltutionTitle>
+                  <Row justifyContent="center">
+                    <EvoltuionImages source="/digimons/greymon.jpg" />
+                    <EvoltuionImages source="/digimons/geogreymon.jpg" />
+                    <EvoltuionImages source="/digimons/meramon.jpg" />
+                    <EvoltuionImages source="/digimons/tyrannomon.jpg" />
+                  </Row>
+                </EvolutionContainer>
               </Row>
-            </EvolutionContainer>
+            </CardContainer>
           </Row>
-        </CardContainer>
-      </Row>
+        </>
+      )}
     </>
   );
 }
