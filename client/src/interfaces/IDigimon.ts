6f558bdbbbecbb3e71c0143249beca7a0269edf6
matheus <matheus.xmaz10@gmail.com>
2021-01-13 11:18:52 -0300

    Interfaces on client

---
+++ client/src/interfaces/IDigimon.ts
@@ -0,0 +1,19 @@
+import ISkill from "./ISkill";
+
+interface IDigimon {
+  id: string;
+  name: string;
+  stage: string;
+  type: string;
+  memory: string;
+  requirements: string;
+  attribute: string;
+  equipSlot: string;
+  supSkillName: string;
+  supSkillDescribe: string;
+  digivolveFrom: string[];
+  digivolveInto: string[];
+  skills: ISkill[];
+}
+
+export default IDigimon;
---
+++ client/src/interfaces/ISkill.ts
@@ -0,0 +1,12 @@
+interface ISkill {
+    level: string;
+    name: string;
+    sp: string;
+    type: string;
+    power: string;
+    attribute: string;
+    inherit: string;
+    description: string;
+}
+
+export default ISkill;
